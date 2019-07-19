6abf8ebd4fa99e21cf3774c8069ec137a8c63ef2
Ruby King <ruby.sz.king@gmail.com>
2019-07-19 13:40:08 -0400

    toggle faves added

--- app.js
+++ app.js
@@ -7,14 +7,12 @@ $(() => {
         $.each(dataResult, (index, eachResult) => {
             if (eachResult.synonyms && index < 10) {
                 for (let i = 0; i < eachResult.synonyms.length; i++) {
-                    console.log('got:',eachResult.synonyms, index);
                     const $new = $('<a>').text(`${eachResult.synonyms[i]}`)
                         if (eachResult.synonyms[i].indexOf(' ') >= 0){
                             $new.splice([1])
                         } else { 
                             handleSynClick($new)
                             $synDiv.append($new)
-                            console.log('new',$new.val()); 
                         }
                 }
             } else {
@@ -23,18 +21,22 @@ $(() => {
         })
         
         const $definition = $('<h2>').text(`${data.results[0].definition}`)
-        const $partOfSpeech = $('<h4>').text(data.results[0].partOfSpeech)
+        const $partOfSpeech = $('<h4>').text(`${data.results[0].partOfSpeech}`)
         const $pronunciation = $('<h5>').text(`{${data.pronunciation.all}}`)
+        const $dot = $('<h4>').text('•')
 
-        $('.container').append($pronunciation, $partOfSpeech, $definition, $synDiv); 
+        $('.container').append($pronunciation, $dot, $partOfSpeech, $definition, $synDiv); 
     }
 
     /////////////// EVENT LISTENERS ////////////////
+
+    let counter = 0;
 
     $('form').on('submit', (event) => {
         event.preventDefault()
         $('.container').empty(); 
         const input = $('.word').val()
+        $('.heart').css('display', 'block')
         const BASE_URL = `https://wordsapiv1.p.rapidapi.com`
 
         $.ajax({
@@ -50,8 +52,10 @@ $(() => {
         $(element).on('click', (event) => {
             event.preventDefault()
             $('.container').empty();
-            console.log('thing', element[0].innerText)
+            counter = 0;
+            $('.heart').css('color', 'black');
             const clickedSyn = element[0].innerText;
+            localStorage.setItem('word', clickedSyn);
             $('.word').val(clickedSyn)
             const BASE_URL = `https://wordsapiv1.p.rapidapi.com`
             $.ajax({
@@ -63,5 +67,31 @@ $(() => {
             .then(handleData);
         })
     }
+
+    const saveWord = () => {
+        const input = $('.word').val();
+        localStorage.setItem('word', input);
+        console.log(localStorage);
+    }
+
+    const deleteWord = () => {
+        const input = $('.word').val();
+        localStorage.removeItem('word', input);
+        console.log(localStorage);
+    }
+
+        // let counter = 0;
+
+        $('.heart').on('click', (event) => {
+            event.preventDefault();
+            counter++
+                 if (counter % 2 !== 0) {
+                    $('.heart').css('color', '#ff7d93');
+                    saveWord();
+                } else {
+                    $('.heart').css('color', 'black');
+                    deleteWord();
+                }
+        })
 
 })
